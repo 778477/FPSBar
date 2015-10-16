4f03696ef1ea4663e441bd6ce789383906dab8b7
778477 <1032419360@qq.com>
2015-10-16 11:51:29 +0800

    Initial Commit

---
+++ FPSBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FPSBar
+//
+//  Created by 郭妙友 on 15/10/16.
+//  Copyright © 2015年 miaoyou.gmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FPSBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FPSBar
+//
+//  Created by 郭妙友 on 15/10/16.
+//  Copyright © 2015年 miaoyou.gmy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
